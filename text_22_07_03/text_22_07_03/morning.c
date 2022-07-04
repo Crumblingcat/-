a1607c318bf46588ccee974a8eb3803bb3e0f8dd
Crumblingcat <107230012+Crumblingcat@users.noreply.github.com>
2022-07-04 09:00:26 +0800

    Add files via upload

---
+++ text_22_07_03/text_22_07_03/morning.c
@@ -0,0 +1,88 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int m = 0;
+//	int n = 0;
+//	scanf("%d%d", &m, &n);
+//	int sum = 0;
+//	sum = m + n;
+//	printf("%d", sum);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int i = 2020;
+//
+//void test()
+//{
+//	printf("%d\n", i);
+//}
+//int main()
+//{
+//	test();
+//	printf("%d", i);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//enum Sex
+//{
+//	MALE,
+//	FEMALE,
+//	FECRET
+//};
+//int main()
+//{
+//	printf("%d", MALE);
+//	printf("%d", FEMALE);
+//	printf("%d", FECRET);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//enum Color
+//{
+//	BLUE,
+//	YELLOW,
+//	RED
+//};
+//int main()
+//{
+//	enum Color color = BLUE;
+//	printf("%d", color);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	char arr1[] = "abc";
+//	//"abc" -- 'a','b','c','\0' -- '\0'是字符串结束的标志
+//	char arr2[] = { 'a','b','c' };
+//	//'a','b','c'后面没有'\0'直到遇到停止
+//	printf("%s\n", arr1);
+//	printf("%s", arr2);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a','b','c' };
+//	printf("%d\n", strlen(arr1));
+//	printf("%d", strlen(arr2));//随机值，直到遇见'\0'
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("你说什么\?\n");  //  \? 打印一个？      \n 换行下一段 
+//	printf("\"");    //     \"  打印一个"
+//	printf("546\t123");    //  \t 按下一个tab
+//	printf("123456\b");  //   \b 除去前一个字符
+//	return 0;
+//}
+
